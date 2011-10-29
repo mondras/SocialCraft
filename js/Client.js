a133567ce507f71248c97d498525b02fc7252073
eduardo.vazquez@ikigaming.com <Asus@Asus-PC.(none)>
2011-10-28 20:50:00 -0500

    se crean nuevos players

--- js/Client.js
+++ js/Client.js
@@ -1,11 +1,12 @@
 console.log("Loading client sockets...");
 
 var socket = io.connect('http://'+window.location.host);
+var playerId = "";
 var players = [];
 
 function sendPlayerMove(dx,dy,dz,drx,dry,drz)
 {
-    coords = {id: player.nickname, dx:dx, dy:dy, dz:dz, drx:drx, dry:dry,drz:drz};
+    coords = {id: playerId, dx:dx, dy:dy, dz:dz, drx:drx, dry:dry,drz:drz};
     socket.emit('move',coords);
 }
 
@@ -23,18 +24,29 @@ socket.on('Greeting', function (data) {
         ry: player.rotation.y,
         rz: player.rotation.z
         };
+        playerId = nickname;
         console.log(newPlayer);
         socket.emit('setNickname', newPlayer);
         console.log("init-game");
         });
 
 socket.on('receiveMoves', function (data) {
-        console.log("Someone moved!");
+        //console.log("Someone moved!");
+        var objectPlayer = players[data.id];
+        if (objectPlayer != null)
+        {
+            objectPlayer.position.x += data.dx;
+            objectPlayer.position.y += data.dy;
+            objectPlayer.position.z += data.dz;
 
+            objectPlayer.rotation.x += data.drx;
+            objectPlayer.rotation.y += data.dry;
+            objectPlayer.rotation.z += data.drz;
+        }
 });
 
 socket.on('createNewPlayer', function(data) {
         console.log("A new player has arrived!");
         console.log(data);
-        
+        players[data.id] = createPlayerOnMap(data);
 });
